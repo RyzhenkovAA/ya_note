13b2a056ceede192e95744d682cc3e05b7b430d5
Артём Рыженков <RyizhenkovAA@yandex.ru>
2024-06-11 00:46:17 +0300

    5

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 
 from django.urls import reverse
 
+import pytest
+
 
 # Указываем в фикстурах встроенный клиент.
 def test_home_availability_for_anonymous_user(client):
@@ -9,3 +11,12 @@ def test_home_availability_for_anonymous_user(client):
     url = reverse('notes:home')
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK 
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.O
