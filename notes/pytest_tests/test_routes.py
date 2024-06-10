79a95358485bc21c61485e3a0cb1a278ce275a9f
Артём Рыженков <RyizhenkovAA@yandex.ru>
2024-06-11 00:33:41 +0300

    4

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,11 @@
+from http import HTTPStatus
+
+from django.urls import reverse
+
+
+# Указываем в фикстурах встроенный клиент.
+def test_home_availability_for_anonymous_user(client):
+    # Адрес страницы получаем через reverse():
+    url = reverse('notes:home')
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK 
